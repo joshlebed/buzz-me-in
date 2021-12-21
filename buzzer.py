96f0e7bff7941841aa0741b35bca808ca5f388cb
Josh Lebedinsky <joshlebed@gmail.com>
2021-12-21 14:34:20 -0500

    clean up buzzer

--- buzzer.py
+++ buzzer.py
@@ -13,13 +13,11 @@ import utils
 BASE_URL = "https://api.switch-bot.com/"
 
 
-def print_response(response):
-    print(json.dumps(json.loads(response.text), indent=2))
-
-
 @limits(calls=1, period=5)
 def send_buzz(switchbot_auth):
+    """send request to buzzer"""
     utils.log("buzzing")
+    # TODO: get this id from config file, or let admin configure via sms
     buzzer_bot_id = "FEEC403B37A8"
     post_buzz_url = BASE_URL + "v1.0/devices/" + buzzer_bot_id + "/commands"
     body = {"command": "turnOn", "parameter": "default", "commandType": "command"}
