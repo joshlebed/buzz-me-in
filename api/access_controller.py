55e5a17c5e40c7eb6e953f415a00fa6f081e57b1
Josh Lebedinsky <joshlebed@gmail.com>
2022-01-22 19:07:10 -0500

    add error codes and hardcoded access code

--- api/access_controller.py
+++ api/access_controller.py
@@ -1,7 +1,11 @@
+import time
+
 from flask import current_app as app
 
 import buzzer
 import utils
+
+hardcoded_access_codes = ["let me in"]
 
 
 def handle_unlock_request(access_code):
@@ -9,19 +13,27 @@ def handle_unlock_request(access_code):
     if access_code == "":
         return utils.bad_request_response()
 
-    if access_code.lower().strip() == "1":
+    if access_code.lower().strip() in hardcoded_access_codes:
         try:
             response = buzzer.send_buzz(app.config["SWITCHBOT_AUTH"])
             utils.log(response)
             if not response or response.status_code != 200:
                 raise Exception("failed to buzz")
 
-            return {"success": True, "message": "buzzed"}
+            # this is bad programming... maybe do it client side instead
+            time.sleep(2)
+
+            return {"success": True, "errorCode": 0, "message": "buzzed"}
 
         except utils.RequestFailedException:
-            return {"success": False, "message": "failed to reach buzzer :("}
+            return {
+                "success": False,
+                "errorCode": 3,
+                "message": "failed to reach buzzer :(",
+            }
 
         except Exception:
-            return {"success": False, "message": "failed to buzz"}
+            return {"success": False, "errorCode": 2, "message": "failed to buzz"}
 
-    return {"success": False, "message": "invalid access code"}
+    time.sleep(2)
+    return {"success": False, "errorCode": 1, "message": "invalid access code"}
