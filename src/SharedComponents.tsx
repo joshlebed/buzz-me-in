e8fb4680e961b134750d73a9a664e0fec66db8f3
Josh Lebedinsky <joshlebed@gmail.com>
2022-01-22 19:18:56 -0500

    hotfix

--- src/SharedComponents.tsx
+++ src/SharedComponents.tsx
@@ -1,5 +1,5 @@
 import LoadingOverlay from 'react-loading-overlay';
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 
 const UnlockButtonContainer = styled.div`
   display: flex;
